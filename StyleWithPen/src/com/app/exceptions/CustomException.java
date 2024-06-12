2e2b9cd9e3d181c931ef069f9e6bbd906042fe09
KajalN22 <166602217+KajalN22@users.noreply.github.com>
2024-06-13 01:11:10 +0530

    Add files via upload

---
+++ StyleWithPen/src/com/app/exceptions/CustomException.java
@@ -0,0 +1,9 @@
+package com.app.exceptions;
+
+public class CustomException extends Exception {
+
+	public CustomException(String errmsg) {
+		// TODO Auto-generated constructor stub
+	super(errmsg);
+	}
+}
