01c41ae1db78be0ea8ee6de9ef839cafb5575490
KajalN22 <166602217+KajalN22@users.noreply.github.com>
2024-07-06 21:22:51 +0530

    Delete StyleWithPen directory

--- StyleWithPen/src/com/app/exceptions/CustomException.java
+++
@@ -1,9 +0,0 @@
-package com.app.exceptions;
-
-public class CustomException extends Exception {
-
-	public CustomException(String errmsg) {
-		// TODO Auto-generated constructor stub
-	super(errmsg);
-	}
-}
